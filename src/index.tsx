2f52eca4761f5644e1daf111d08ac2574f9d08c6
kamil <you@example.com>
2022-09-01 19:40:30 +0200

    index refactor

--- src/index.tsx
+++ src/index.tsx
@@ -1,21 +1,21 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
-import { ChakraProvider } from '@chakra-ui/react'
-import {BrowserRouter} from "react-router-dom";
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
+import { ChakraProvider } from "@chakra-ui/react";
+import { BrowserRouter } from "react-router-dom";
 
 const root = ReactDOM.createRoot(
-  document.getElementById('root') as HTMLElement
+  document.getElementById("root") as HTMLElement
 );
 root.render(
   <React.StrictMode>
-      <BrowserRouter>
-          <ChakraProvider>
-              <App />
-          </ChakraProvider>
-      </BrowserRouter>
+    <BrowserRouter>
+      <ChakraProvider>
+        <App />
+      </ChakraProvider>
+    </BrowserRouter>
   </React.StrictMode>
 );
 
@@ -31,3 +31,6 @@ reportWebVitals();
 
 //TODO usememo
 //TODO usecallback
+
+//TODO suspense loading
+//TODO lazy loading
