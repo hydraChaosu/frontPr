e3b83dc46c9f665f7715e251c79ab139b1417d82
kamil <you@example.com>
2022-09-01 16:11:31 +0200

    not found rework

--- src/views/NotFound.tsx
+++ src/views/NotFound.tsx
@@ -1,7 +1,12 @@
 import React from "react";
+import {Box, Flex, Image} from "@chakra-ui/react";
 
-const NotFound = () => {
-  return <p>Error 404 page not found</p>;
-};
-
-export default NotFound;
+export default function NotFoundPage() {
+  return (
+      <Flex bg="#222224" alignItems="center" justifyContent="center" h="100vh">
+        <Box>
+          <Image src='https://http.cat/404' alt='page not found 404' />
+        </Box>
+      </Flex>
+  );
+}
