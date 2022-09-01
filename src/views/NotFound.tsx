b3c8ae83e9a23d11483288de88b7e0ec3aec1af1
kamil <you@example.com>
2022-09-01 17:03:54 +0200

    finished not found page

--- src/views/NotFound.tsx
+++ src/views/NotFound.tsx
@@ -1,12 +1,17 @@
 import React from "react";
-import {Box, Flex, Image} from "@chakra-ui/react";
+import { Box, Flex, Image } from "@chakra-ui/react";
 
 export default function NotFoundPage() {
   return (
-      <Flex bg="#222224" alignItems="center" justifyContent="center" h="100vh">
-        <Box>
-          <Image src='https://http.cat/404' alt='page not found 404' />
-        </Box>
-      </Flex>
+    <Flex
+      bg="#222224"
+      alignItems="center"
+      justifyContent="center"
+      h="calc(100vh - 80px)"
+    >
+      <Box>
+        <Image src="https://http.cat/404" alt="page not found 404" />
+      </Box>
+    </Flex>
   );
 }
