36db6e9d8ef6af375a1054075e51cbbbdef6fcc0
kamil <you@example.com>
2022-09-01 16:12:17 +0200

    header init

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,14 +1,63 @@
-import React from 'react';
-import {Link, NavLink} from "react-router-dom";
+import React, {useState} from 'react';
+import {NavLink, useLocation} from "react-router-dom";
+import {
+    Breadcrumb,
+    BreadcrumbItem,
+    BreadcrumbLink,
+    BreadcrumbSeparator, Flex, Spacer,
+} from '@chakra-ui/react'
 
 export const Header = () => {
+    //if logged in see user and not reg and login
+    //if not see reg and login
+    const [loggedIn, setLoggedIn] =useState(false)
+
+    const {pathname} = useLocation()
     const colorOfLink = ({isActive}:{isActive: boolean}) => ({color: isActive ? 'green' : 'red'})
+
+
 
     return(
         <>
-            <h1>Santa App</h1>
-            Menu: <NavLink style={colorOfLink} to="/gift">Gifts</NavLink> | <NavLink style={colorOfLink}
-                                                                                     to="/child">Children</NavLink>
-            <hr/>
+            <Flex paddingRight="10px" justify="right">
+            <Breadcrumb>
+                <BreadcrumbItem>
+                    <BreadcrumbLink as={NavLink} to='/'>
+                        Home
+                    </BreadcrumbLink>
+                </BreadcrumbItem>
+                <BreadcrumbItem>
+                    <BreadcrumbLink as={NavLink} to='/basket'>
+                        Basket
+                    </BreadcrumbLink>
+                </BreadcrumbItem>
+                {loggedIn &&
+                    (
+                    <BreadcrumbItem>
+                    <BreadcrumbLink as={NavLink} to='/login'>
+                        Login
+                    </BreadcrumbLink>
+                </BreadcrumbItem>
+                    )}
+                {loggedIn && (
+                <BreadcrumbItem>
+                    <BreadcrumbLink as={NavLink} to='/register'>
+                        Register
+                    </BreadcrumbLink>
+                </BreadcrumbItem>
+
+                )}
+                {loggedIn && (
+                <BreadcrumbItem>
+                    <BreadcrumbLink as={NavLink} to='/user'>
+                        User
+                    </BreadcrumbLink>
+                </BreadcrumbItem>
+
+                )}
+
+
+            </Breadcrumb>
+            </Flex>
         </>
 )}
