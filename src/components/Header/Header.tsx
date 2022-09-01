2764ea6298048b471be6048a13d6f122d1574fe8
kamil <you@example.com>
2022-09-01 19:41:29 +0200

    header done

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,63 +1,53 @@
-import React, {useState} from 'react';
-import {NavLink, useLocation} from "react-router-dom";
+import React, { useState } from "react";
+import { NavLink, useLocation } from "react-router-dom";
 import {
-    Breadcrumb,
-    BreadcrumbItem,
-    BreadcrumbLink,
-    BreadcrumbSeparator, Flex, Spacer,
-} from '@chakra-ui/react'
+  Breadcrumb,
+  BreadcrumbItem,
+  BreadcrumbLink,
+  BreadcrumbSeparator,
+  Flex,
+} from "@chakra-ui/react";
+import { UserMenu } from "./UserMenu";
 
 export const Header = () => {
-    //if logged in see user and not reg and login
-    //if not see reg and login
-    const [loggedIn, setLoggedIn] =useState(false)
+  //if logged in see user and not reg and login
+  //if not see reg and login
+  const [loggedIn, setLoggedIn] = useState(false);
 
-    const {pathname} = useLocation()
-    const colorOfLink = ({isActive}:{isActive: boolean}) => ({color: isActive ? 'green' : 'red'})
+  const { pathname } = useLocation();
+  const colorOfLink = ({ isActive }: { isActive: boolean }) => ({
+    color: isActive ? "green" : "red",
+  });
 
-
-
-    return(
-        <>
-            <Flex paddingRight="10px" justify="right">
-            <Breadcrumb>
-                <BreadcrumbItem>
-                    <BreadcrumbLink as={NavLink} to='/'>
-                        Home
-                    </BreadcrumbLink>
-                </BreadcrumbItem>
-                <BreadcrumbItem>
-                    <BreadcrumbLink as={NavLink} to='/basket'>
-                        Basket
-                    </BreadcrumbLink>
-                </BreadcrumbItem>
-                {loggedIn &&
-                    (
-                    <BreadcrumbItem>
-                    <BreadcrumbLink as={NavLink} to='/login'>
-                        Login
-                    </BreadcrumbLink>
-                </BreadcrumbItem>
-                    )}
-                {loggedIn && (
-                <BreadcrumbItem>
-                    <BreadcrumbLink as={NavLink} to='/register'>
-                        Register
-                    </BreadcrumbLink>
-                </BreadcrumbItem>
-
-                )}
-                {loggedIn && (
-                <BreadcrumbItem>
-                    <BreadcrumbLink as={NavLink} to='/user'>
-                        User
-                    </BreadcrumbLink>
-                </BreadcrumbItem>
-
-                )}
-
-
-            </Breadcrumb>
-            </Flex>
-        </>
-)}
+  return (
+    <Flex paddingRight="10px" align="center" justify="right" h="80px" w="100vw">
+      <Breadcrumb>
+        <BreadcrumbItem>
+          <BreadcrumbLink as={NavLink} to="/">
+            Home
+          </BreadcrumbLink>
+        </BreadcrumbItem>
+        <BreadcrumbItem>
+          <BreadcrumbLink as={NavLink} to="/basket">
+            Basket
+          </BreadcrumbLink>
+        </BreadcrumbItem>
+        {!loggedIn && (
+          <BreadcrumbItem>
+            <BreadcrumbLink as={NavLink} to="/login">
+              Login
+            </BreadcrumbLink>
+          </BreadcrumbItem>
+        )}
+        {!loggedIn && (
+          <BreadcrumbItem>
+            <BreadcrumbLink as={NavLink} to="/register">
+              Register
+            </BreadcrumbLink>
+          </BreadcrumbItem>
+        )}
+      </Breadcrumb>
+      {loggedIn && <UserMenu />}
+    </Flex>
+  );
+};
