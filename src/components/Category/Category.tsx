5bfe3a559efea9339cc5e1184cd8660d33b6f7e5
kamil <you@example.com>
2022-09-04 14:49:15 +0200

    done category

--- src/components/Category/Category.tsx
+++ src/components/Category/Category.tsx
@@ -1,10 +1,26 @@
-import {CategoryItem} from "./CategoryItem";
+import { CategoryItem } from "./CategoryItem";
+import { Box, Flex } from "@chakra-ui/react";
 
 export function Category() {
-    //show category items to filter
-    return (
-        <div>
-            {['all', 'alcohol','apples'].map( item => <CategoryItem name={item}/>)}
-        </div>
-    );
+  //show category items to filter
+  return (
+    <Flex
+      bg="gray.100"
+      // align="center"
+      // justify="center"
+      h="calc(100vh - 80px)"
+      direction="column"
+      minW="20vw"
+      borderRight="1px"
+      borderRightColor="black"
+    >
+      <Box bg="tomato" w="100%" p={4} color="white" textAlign="center">
+        Categories
+      </Box>
+      {/* //onClicklater*/}
+      {["all", "alcohol", "apples"].map((item) => (
+        <CategoryItem name={item} />
+      ))}
+    </Flex>
+  );
 }
