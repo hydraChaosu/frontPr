51fc9430f15ae8d1c7aaa9a1c7e4148c9440bdf7
kamil <you@example.com>
2022-09-04 14:49:25 +0200

    done category item

--- src/components/Category/CategoryItem.tsx
+++ src/components/Category/CategoryItem.tsx
@@ -1,5 +1,16 @@
+import { Box } from "@chakra-ui/react";
+
 export function CategoryItem(props: { name: string }) {
-    return (
-        <p>{props.name}</p>
-    );
+  return (
+    <Box
+      as="button"
+      bg="white"
+      w="100%"
+      p={4}
+      color="tomato"
+      _hover={{ bg: "gray.200" }}
+    >
+      {props.name}
+    </Box>
+  );
 }
